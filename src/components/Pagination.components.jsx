a96eca91d68af08fdcbd3fbe31d01a1b9ae3e29a
waiyanhein01 <wyan00196@gmail.com>
2024-10-16 03:07:07 +0630

    pagination

--- src/components/Pagination.components.jsx
+++ src/components/Pagination.components.jsx
@@ -10,7 +10,7 @@ const PaginationComponents = ({links:{next,prev},meta:{from,to,total},fetchUrlHa
   return (
     <div className="flex justify-between items-center mt-3">
       <span className="text-sm text-gray-700 dark:text-gray-400">
-        Showing <b className="font-semibold text-gray-900 dark:text-white">{from}</b> to <b>{to}</b> of <b>{total}</b> Entries
+        Showing <b className="font-semibold text-gray-900 dark:text-white">{from}</b> to <b className="font-semibold text-gray-900 dark:text-white">{to}</b> of <b className="font-semibold text-gray-900 dark:text-white">{total}</b> Entries
       </span>
       <div className="inline-flex mt-2 xs:mt-0">
         <button onClick={handlePrevHandler} disabled={!prev} className="flex items-center justify-center disabled:pointer-events-none disabled:opacity-50 px-4 py-2 text-sm font-medium text-gray-900 bg-gray-50 rounded-s-lg hover:bg-gray-200 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
